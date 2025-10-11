9c32af3db778bbdb3ca32ecea9c359695c5f4e37
Raghul <rexizzz1815@gmail.com>
2025-10-11 21:29:35 +0530

    Create Classes_Objects.java
    Today I have been practiced the Classes and Objects Successfully

---
+++ Classes_Objects.java
@@ -0,0 +1,31 @@
+package oop_practice;
+class Car{
+	String brand;
+	String model;
+	int price;
+
+void showDetails() {
+	System.out.println("Brand : "+brand);
+	System.out.println("Model : "+model);
+	System.out.println("Price : "+price);
+	System.out.println();
+}
+}
+public class Classes_Objects {
+
+	public static void main(String[] args) {
+      Car Audi = new Car();
+      Audi.brand = ("Audi");
+      Audi.model = "2006 Model";
+      Audi.price = 123456;
+      Audi.showDetails();
+      
+      Car BMW = new Car();
+      BMW.brand = "BMW";
+      BMW.model = "2007 model";
+      BMW.price = 123456;
+      BMW.showDetails();
+	
+
+}
+}
