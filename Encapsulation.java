dce3d1ae89d567d71d8d1d52658ae0f95e85508c
Raghul <rexizzz1815@gmail.com>
2025-10-15 22:10:20 +0530

    Create Encapsulation.java
    This is the simple example program for the encapsulation in the oop concept

---
+++ Encapsulation.java
@@ -0,0 +1,35 @@
+package oop_practice;
+class BankAccount{
+	private String accountHolder;
+	private double amount;
+	
+	public void setAccountHolder(String name) {
+		accountHolder = name;
+		
+	}
+	public void setAmount(double a) {
+		if(amount>=0)
+		amount = a;
+		else
+			System.out.println("Invalid amount in the account");
+	}
+	public String getAccountHolder() {
+		return accountHolder;
+	}
+	public double getAmount() {
+		return amount;
+	}
+}
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		BankAccount bbt = new BankAccount();
+		bbt.setAccountHolder("Raghul");
+		bbt.setAmount(5000);
+		
+		System.out.println("User name : " + bbt.getAccountHolder());
+		System.out.println("Amount having in the Account : "+ bbt.getAmount());
+
+	}
+
+}
