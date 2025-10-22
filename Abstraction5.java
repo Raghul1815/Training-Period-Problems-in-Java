901b8f39e1867280779b647eac86a66b2b709daf
Raghul <rexizzz1815@gmail.com>
2025-10-22 15:02:34 +0530

    Create Abstraction5.java
    This is the example program for the abstraction with the super method calling and the with the Parameterized Constructor

---
+++ Abstraction5.java
@@ -0,0 +1,31 @@
+abstract class Vehiclee{
+String brand;
+	
+	Vehiclee(String b){
+		brand = b;
+		System.out.println("Vehicle Constructor is called");
+	}
+	abstract void start();
+}
+
+class Cares extends Vehiclee{
+	int year;
+	
+	Cares(String b,int y){
+		super(b);
+		year = y;
+		System.out.println("Car Constructor is called");
+	}
+	void start() {
+		System.out.println(brand + " Car started in year " + year);
+	}
+}
+public class Abstraction5 {
+
+	public static void main(String[] args) {
+		Cares cc = new Cares("BMW",1997);
+		cc.start();
+
+	}
+
+}
