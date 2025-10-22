e107f3116a7ac565edce2489c5c5ce22caf59738
Raghul <rexizzz1815@gmail.com>
2025-10-22 15:00:44 +0530

    Create Abstraction4.java
    This is them example program for the abstraction with the hierarchical inheritance

---
+++ Abstraction4.java
@@ -0,0 +1,56 @@
+abstract class Shape{
+	abstract double area();
+	
+	void displayShape() {
+		System.out.println("This is a shape");
+	}
+}
+class Circle extends Shape{
+	double radius;
+	
+	Circle(double r){
+		radius = r;	
+	}
+	double area() {
+		return Math.PI * radius * radius;
+	}
+}
+class Rectangle extends Shape{
+	double length,width;
+	
+	Rectangle(double l,double w){
+		length = l;
+		width = w;
+	}
+	double area() {
+		return length * width;
+	}
+}
+public class Abstraction4 {
+
+	public static void main(String[] args) {
+		Shape cc;
+		
+		cc = new Circle(2);
+		cc.displayShape();
+		System.out.println(cc.area());
+		System.out.println();
+		cc = new Rectangle(3,4);
+		cc.displayShape();
+		System.out.println(cc.area());
+		
+		System.out.println();
+		System.out.println();	
+//		(or)
+		Shape c = new Circle(23);
+		Shape r = new Rectangle(2,3);
+	
+		c.displayShape();
+		System.out.println("Circle area: " + c.area());
+        System.out.println();
+        
+        r.displayShape();
+        System.out.println("Rectangle area: " + r.area());
+	}
+
+}
