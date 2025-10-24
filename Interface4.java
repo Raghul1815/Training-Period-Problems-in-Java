d42fe58cb79f006ce035b29f66d22b5914fbe359
Raghul <rexizzz1815@gmail.com>
2025-10-24 22:49:14 +0530

    Create Interface4.java
    This is the example for the Interface with the Combinations of the Inheritance and the Method calling methods...

---
+++ Interface4.java
@@ -0,0 +1,32 @@
+package oop_practice;
+interface Mobiless{
+	void camera();
+	void music();
+}
+
+abstract class Device{
+	abstract void powerOn();
+	void charging() {
+		System.out.println("mobile is charging");
+	}
+}
+class Smartphones extends Device implements Mobiless{
+	public void camera(){
+		System.out.println("This is the camera in mobile");
+	}
+	public void music() {
+		System.out.println("We can also play the music in the mobile phone too");
+	}
+	void powerOn() {
+		System.out.println("This is the Power on symbol");
+	}
+}
+public class Interface4 {
+public static void main(String[] args) {
+	Smartphones sms = new Smartphones();
+	sms.camera();
+	sms.music();
+	sms.charging();
+	sms.powerOn();
+}
+}
